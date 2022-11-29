7699a926528c9e11ba8b0dc50b590f21b91d2b1b
JohanThomsen <jthoms19@student.aau.dk>
2022-11-29 11:39:48 +0100

    cpuFreq no longer random

--- cpuFreq.py
+++ cpuFreq.py
@@ -3,12 +3,12 @@ import re
 import random
 
 def set_cpu_freq():
+    random.seed(42)
     info = os.popen('cpufreq-info').read()
     frequencies_text = re.search('available frequency steps: (.+)\n', info).group(1)
     frequencies = list(map(lambda x: x.replace(' ', ''), frequencies_text.split(',')))
     chosen_frequency = random.choice(frequencies)
     os.popen(f'sudo cpufreq-set -g userspace')
     os.popen(f'sudo cpufreq-set -f {chosen_frequency}')
-
 
 set_cpu_freq()
